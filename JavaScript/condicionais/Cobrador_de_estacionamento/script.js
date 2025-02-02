a283a48fcc5ee6352f71f54f315b1813b9046bd4
Marcos Roberto <mr.codeengineer@gmail.com>
2025-02-02 20:09:39 -0300

    atualização da documentação do programa.

--- JavaScript/condicionais/Cobrador_de_estacionamento/script.js
+++ JavaScript/condicionais/Cobrador_de_estacionamento/script.js
@@ -1,57 +1,60 @@
 
 
+let  = null;
+
 //Essa função é responsavel por registrar a placa e o horario da entrada do veiculo no estacionamento!
 //função que é chamada quando o botão de entrada de veiculo for acionado.
-let funçaoEntrada = null;
-
-
 function entrada(){
+    //recebe a valor digitado no campo de placa e armazena na variavel placa
+    var placa = document.getElementById('placa-do-carro').value.toUpperCase();
 
-    var placa = document.getElementById('placa-do-carro').value.toUpperCase(); // captura o valor dentro do elemento selecionado pelo id.
-    var retornarPlaca = document.getElementById("retornar-placa")            //captura o elemento selecionado pelo id
-    var retornarHoras = document.getElementById("retorna-horario-de-entrada")
 
+    //captura os elementos que serão alterados no html e armazena em variaveis
+    var retornarPlaca = document.getElementById("retornar-placa")            
+    var retornarHoras = document.getElementById("horario-de-entrada")
 
 
     //verificador do campo destinado a placa: se vazio, então ele retorna um alerta, caso receba um valor, executa o proximo bloco
-    if (placa == ""){
-    alert("Por favor, digite a placa do veiculo!")
+    if (placa !== ""){        
+        const agora = new Date();//cria um objeto com a data e hora atual
+        const horas = agora.getHours(); //captura a hora atual
+        const minutos= agora.getMinutes(); //captura os minutos atuais
+        const segundos = agora.getSeconds(); //captura os segundos atuais
+        const indice = [horas,":", minutos,":", segundos] //constante que armazena os valores das variaveis de cima em arrays.
 
-    }else{
-        //bloco que captura o horario atual é salva em uma variavel.
-        const agora = new Date();
-        const horas = agora.getHours();
-        const minutos= agora.getMinutes();
-        const segundos = agora.getSeconds();
-        const indice = [horas,":", minutos,":", segundos]
 
         //constante que armazena os valores das variaveis de dentro da função em arrays.
-        const retornoF1 = [placa, indice]
-
+        //const retornoF1 = [placa, indice]
         retornarPlaca.innerHTML = `<p>Placa do veiculo: <strong>${placa}</strong></p>`
         retornarHoras.innerHTML = `Entrada registrada as ${indice[0]}:${indice[2]}:${indice[4]}`
-        return retornoF1;
+        return [placa, indice] //retorna a placa e o horario de entrada do veiculo quando a função é chamada.}
+
+    } else {
+        alert("Por favor, digite a placa do veiculo!") 
     }                 
 }
 
-document.getElementById("entrada").onclick = function(){
-    if(funçaoEntrada==null){
-        funçaoEntrada = entrada()}}  
+
+//Executa uma função quando o botão "registrar entrada" for acionado
+document.getElementById("entrada").onclick = function(){    
+
+        // a variavel funçaoEntrada recebe o retorno da função entrada()
+        funçaoEntrada = entrada()} 
 
 
+//Executa uma função quando o botão "registrar saida" for acionado
 document.getElementById("saida").onclick = function(){
 
-   
-    var retornarHoras2 = document.getElementById("retorna-horario-de-saida");
+    const agora = new Date(); //cria um objeto com a data e hora atual
+    const horas = agora.getHours(); //captura a hora atual
+    const minutos= agora.getMinutes(); //captura os minutos atuais
+    const segundos = agora.getSeconds();//captura os segundos atuais
+    const indice2 = [horas,":", minutos,":", segundos] //constante que armazena os valores das variaveis de cima em arrays.   
+
+
+    //captura os elementos que serão alterados no html e armazena em variaveis
+    var retornarHoras2 = document.getElementById("horario-de-saida");
     var valorAPagar = document.getElementById("valor-a-pagar");
-
-
-    const agora = new Date();
-    const horas = agora.getHours();
-    const minutos= agora.getMinutes();
-    const segundos = agora.getSeconds();
-    const indice2 = [horas,":", minutos,":", segundos]    
-
     retornarHoras2.innerHTML = `Saida registrada as ${indice2[0]}:${indice2[2]}:${indice2[4]}`    
 
     //bloco para impedir que o botao registrar saida seja acionado antes de registrar a entrada.
